24f1da36dfa30793085bf44d34f0b9532e58cc03
Derek <orrde01@ca.com>
2019-07-10 10:47:16 -0700

    new file:   gcpdemo/API Gateway and LAC on GCP, Kubernetes v0.2.docx 	new file:   gcpdemo/API Gateway and LAC on GCP, Kubernetes v1.0.docx 	new file:   gcpdemo/Deploy Gateway 9.4 to local Kubernetes Minikube.docx 	new file:   gcpdemo/README.md 	new file:   gcpdemo/build-1-node-cluster.sh 	new file:   gcpdemo/container-gateway-config.yml 	new file:   gcpdemo/container-gateway-mysql.yml 	new file:   gcpdemo/container-gateway.yml 	new file:   gcpdemo/deploy.sh 	new file:   gcpdemo/lac-nps.yml 	new file:   gcpdemo/lac-policy.xml 	new file:   gcpdemo/lac-policy.xml-orig 	new file:   gcpdemo/lac.yml 	new file:   gcpdemo/publish-to-lac-notes.txt
    	CA Internal_4349302617878398853_SSG_Enterprise_9.xml.gz
    	Deploy_on_minikube.docx
    	Deploy_on_minikube_v1.0.docx
            license.xml.gz
    	license.xml.gz.base64
    	~$ploy_on_minikube_v1.0.docx

---
+++ gcpdemo/README.md
@@ -0,0 +1,25 @@
+# plano-k8s
+
+Kubernetes Project for Plano Apr 2019
+
+Clone the project:
+
+```
+git clone https://github.com/matkeller/plano-k8s.git
+```
+
+Fix permissions and run:
+
+```
+cd plano-k8s
+chmod +x deploy.sh build-1-node-cluster.sh
+
+# Deploy with script:
+./deploy.sh
+```
+
+Update your copy:
+
+```
+git pull
+```
---
+++ gcpdemo/deploy.sh
@@ -0,0 +1,31 @@
+echo INFO: Deploying Gateway and MySQL
+kubectl apply -f container-gateway-config.yml -f container-gateway-mysql.yml -f container-gateway.yml --validate=false
+
+echo INFO: Please wait 4.5 minutes for Gateway to start...
+sleep 270s
+
+echo INFO: Deploying Live API Creator
+kubectl apply -f lac-nps.yml --validate=false
+sleep 20s
+
+echo   
+echo kubectl get service
+kubectl get service
+
+echo  
+echo kubectl get pods
+kubectl get pods
+
+echo
+echo INFO:  The following line only works on Google Cloud
+echo gcloud compute instances list
+gcloud compute instances list
+
+echo
+echo INFO: Pushing policy to Gateway via RESTMAN call
+curl -k -u admin:CAdemo123! -H "Content-Type: application/xml" -X POST -d @lac-policy.xml https://34.83.15.101:31843/restman/1.0/services
+sleep 10s
+
+echo
+echo INFO: Calling the deployed gateway service to proxy to Live API Creator!
+curl -k -u admin:CAdemo123! https://34.83.15.101:31843/lac-policy
